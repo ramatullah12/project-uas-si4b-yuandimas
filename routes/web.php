8eebf2b36f83469d18b4a46bf7497562e6165b96
ramatullah12 <yuantullah@gmail.com>
2024-06-25 12:48:45 +0700

    pust

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,16 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('category', CategoryController::class);
-Route::resource('rute', RuteController::class);
-Route::resource('transportasi', TransportasiController::class);
-Route::resource('home', HomeController::class);
-Route::resource('pemesanan', PemesananController::class);
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware(['auth', 'verified']);
+
 
 Route::middleware('auth')->group(function () {
+    Route::resource('category', CategoryController::class);
+    Route::resource('rute', RuteController::class);
+    Route::resource('transportasi', TransportasiController::class);
+    Route::resource('home', HomeController::class);
+    Route::resource('pemesanan', PemesananController::class);
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware(['auth', 'verified']);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
